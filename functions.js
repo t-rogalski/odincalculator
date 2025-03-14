1c1ca9fa1fff18be87e88000215339e26e20a1ce
t-rogalski <tomaszrogalski04@gmail.com>
2025-03-14 20:03:41 +0100

    Dodanie funkcjonalności
    Wynik nie mieści się na ekranie

--- functions.js
+++ functions.js
@@ -1,21 +1,3 @@
-function add(a, b) {
-    return a + b;
-}
-
-function substract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(a, b, symbol) {}
-
 const numbers = document.querySelectorAll(".number");
 const display = document.querySelector(".result");
 const clear = document.querySelector(".clear");
@@ -23,8 +5,25 @@ const backspace = document.querySelector(".backspace");
 const plusminus = document.querySelector(".plusminus");
 const dot = document.querySelector(".dot");
 
+const operations = document.querySelectorAll(".operation");
+const equal = document.querySelector(".equal");
+
+let memory = 0;
+let operand = "";
+let newNumber = false;
+
+function clearall() {
+    memory = 0;
+    operand = "";
+    display.textContent = "0";
+}
+
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
+        if (newNumber) {
+            display.textContent = "0";
+            newNumber = false;
+        }
         if (display.textContent.length <= 11) {
             if (display.textContent == "0") {
                 display.textContent = number.textContent;
@@ -36,7 +35,7 @@ numbers.forEach((number) => {
 });
 
 clear.addEventListener("click", () => {
-    display.textContent = "0";
+    clearall();
 });
 
 backspace.addEventListener("click", () => {
@@ -62,3 +61,63 @@ dot.addEventListener("click", () => {
         }
     }
 });
+
+operations.forEach((operation) => {
+    operation.addEventListener("click", () => {
+        newNumber = true;
+        if (operand == "") {
+            memory = parseFloat(display.textContent);
+            operand = operation.textContent;
+        } else {
+            num = parseFloat(display.textContent);
+            operate(memory, num, operand);
+            operand = operation.textContent;
+        }
+    });
+});
+
+equal.addEventListener("click", () => {
+    num = parseFloat(display.textContent);
+    operate(memory, num, operand);
+    memory = 0;
+    operand = "";
+});
+
+function add(a, b) {
+    display.textContent = a + b;
+}
+
+function substract(a, b) {
+    display.textContent = a - b;
+}
+
+function multiply(a, b) {
+    display.textContent = a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) {
+        display.textContent = "I can't";
+    } else {
+        display.textContent = a / b;
+    }
+}
+
+function operate(a, b, symbol) {
+    switch (symbol) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            substract(a, b);
+            break;
+        case "x":
+            multiply(a, b);
+            break;
+        case "/":
+            divide(a, b);
+            break;
+    }
+    memory = parseFloat(display.textContent);
+    newNumber = true;
+}
