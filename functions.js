b50232c9b3ec12ffdf89cc2e6fb20435065adad2
t-rogalski <tomaszrogalski04@gmail.com>
2025-03-14 18:23:56 +0100

    Pisanie na kalkulatorze
    Bez funkcji

--- functions.js
+++ functions.js
@@ -15,3 +15,48 @@ function divide(a, b) {
 }
 
 function operate(a, b, symbol) {}
+
+const numbers = document.querySelectorAll(".number");
+const display = document.querySelector(".result");
+const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace");
+const plusminus = document.querySelector(".plusminus");
+const dot = document.querySelector(".dot");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        if (display.textContent.length <= 11) {
+            if (display.textContent == "0") {
+                display.textContent = number.textContent;
+            } else {
+                display.textContent = display.textContent + number.textContent;
+            }
+        }
+    });
+});
+
+clear.addEventListener("click", () => {
+    display.textContent = "0";
+});
+
+backspace.addEventListener("click", () => {
+    if (display.textContent.length == 1 && display.textContent != "0") {
+        display.textContent = "0";
+    } else if (display.textContent != "0") {
+        display.textContent = display.textContent.slice(0, -1);
+    }
+});
+
+plusminus.addEventListener("click", () => {
+    if (display.textContent.length <= 11) {
+        display.textContent = -display.textContent;
+    }
+});
+
+dot.addEventListener("click", () => {
+    if (display.textContent.split(".").length - 1 == 0) {
+        if (display.textContent.length <= 11) {
+            display.textContent = display.textContent + dot.textContent;
+        }
+    }
+});
